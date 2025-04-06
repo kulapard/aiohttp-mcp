090587665a22be864c3f5de517c5591dbf855c10
Taras Drapalyuk <taras@drapalyuk.com>
2025-04-06 19:32:39 +0200

    Refactor MCP server implementation and enhance SSE handling

--- aiohttp_mcp/core.py
+++ aiohttp_mcp/core.py
@@ -3,12 +3,45 @@ import logging
 from aiohttp import web
 
 from .registry import mcp
-from .transport import SseServerTransport
+from .transport import EventSourceResponse, SSEServerTransport
 from .utils.discover import discover_modules
 
 __all__ = ["setup_mcp_server"]
 
 logger = logging.getLogger(__name__)
+
+
+class AiohttpMCPServer:
+    """Aiohttp MCP server."""
+
+    __slots__ = ("_path", "_sse")
+
+    def __init__(self, path: str = "/mcp") -> None:
+        self._path = path
+        self._sse = SSEServerTransport(path)
+
+    def setup_routes(self, app: web.Application) -> None:
+        """Setup routes for the MCP server.
+        1. GET /mcp: Handles the SSE connection.
+        2. POST /mcp: Handles incoming messages.
+        """
+        app.router.add_get(self._path, self.handle_sse)
+        app.router.add_post(self._path, self.handle_message)
+
+    async def handle_sse(self, request: web.Request) -> EventSourceResponse:
+        """Handle the SSE connection and start the MCP server."""
+        async with self._sse.connect_sse(request) as sse_connection:
+            await mcp.server.run(
+                read_stream=sse_connection.read_stream,
+                write_stream=sse_connection.write_stream,
+                initialization_options=mcp.server.create_initialization_options(),
+                raise_exceptions=False,
+            )
+        return sse_connection.response
+
+    async def handle_message(self, request: web.Request) -> web.Response:
+        """Handle incoming messages from the client."""
+        return await self._sse.handle_post_message(request)
 
 
 def setup_mcp_server(
@@ -19,19 +52,5 @@ def setup_mcp_server(
     # Go through the discovery process to find all decorated functions
     discover_modules(package_names)
 
-    sse = SseServerTransport(path)
-
-    async def handle_sse(request: web.Request) -> web.Response:
-        async with sse.connect_sse(request) as (read_stream, write_stream):
-            await mcp.server.run(
-                read_stream,
-                write_stream,
-                mcp.server.create_initialization_options(),
-            )
-        return web.Response(text="SSE connection closed", status=200)
-
-    async def handle_messages(request: web.Request) -> web.Response:
-        return await sse.handle_post_message(request)
-
-    app.router.add_get(path, handle_sse)
-    app.router.add_post(path, handle_messages)
+    mcp_server = AiohttpMCPServer(path=path)
+    mcp_server.setup_routes(app)
--- aiohttp_mcp/transport.py
+++ aiohttp_mcp/transport.py
@@ -3,103 +3,126 @@ import uuid
 from collections.abc import AsyncIterator, Awaitable, Callable
 from contextlib import asynccontextmanager
 from dataclasses import dataclass
+from enum import Enum
 from urllib.parse import quote
 from uuid import UUID, uuid4
 
 import anyio
 import mcp.types as types
 from aiohttp import web
-from aiohttp_sse import sse_response
+from aiohttp_sse import EventSourceResponse, sse_response
 from anyio.streams.memory import MemoryObjectReceiveStream, MemoryObjectSendStream
 from pydantic import ValidationError
+
+__all__ = ["EventSourceResponse", "SSEConnection", "SSEServerTransport"]
 
 logger = logging.getLogger(__name__)
 
 
+class EventType(str, Enum):
+    """Event types for SSE."""
+
+    ENDPOINT = "endpoint"
+    MESSAGE = "message"
+
+
 @dataclass
 class Event:
-    event: str
+    """A class to represent an event for SSE."""
+
+    event_type: EventType
     data: str
 
 
-class SseServerTransport:
-    _read_stream_writers: dict[UUID, MemoryObjectSendStream[types.JSONRPCMessage | Exception]]
+@dataclass
+class SSEConnection:
+    """A class to manage the connection for SSE."""
+
+    read_stream: MemoryObjectReceiveStream[types.JSONRPCMessage | Exception]
+    write_stream: MemoryObjectSendStream[types.JSONRPCMessage | Exception]
+    request: web.Request
+    response: EventSourceResponse
+
+
+class SSEServerTransport:
+    _out_stream_writers: dict[UUID, MemoryObjectSendStream[types.JSONRPCMessage | Exception]]
 
     def __init__(self, message_path: str, send_timeout: float | None = None) -> None:
         super().__init__()
         self._message_path = message_path
         self._send_timeout = send_timeout
-        self._read_stream_writers: dict[uuid.UUID, MemoryObjectSendStream[types.JSONRPCMessage | Exception]] = {}
+        self._out_stream_writers: dict[uuid.UUID, MemoryObjectSendStream[types.JSONRPCMessage | Exception]] = {}
+
+    @staticmethod
+    def _ensure_string(msg: types.JSONRPCMessage | Exception) -> str:
+        """Convert message to string."""
+        if isinstance(msg, types.JSONRPCMessage):
+            return msg.model_dump_json(by_alias=True, exclude_none=True)
+        return str(msg)
 
     @asynccontextmanager
-    async def connect_sse(
-        self, request: web.Request
-    ) -> AsyncIterator[
-        tuple[
-            MemoryObjectReceiveStream[types.JSONRPCMessage | Exception],
-            MemoryObjectSendStream[types.JSONRPCMessage | Exception],
-        ]
-    ]:
+    async def connect_sse(self, request: web.Request) -> AsyncIterator[SSEConnection]:
         logger.info("Setting up SSE connection")
 
         # Create memory object streams
-        read_stream_writer, read_stream = anyio.create_memory_object_stream[types.JSONRPCMessage | Exception](0)
-        write_stream, write_stream_reader = anyio.create_memory_object_stream[types.JSONRPCMessage | Exception](0)
+        # Input and output streams
+        in_stream_writer, in_stream_reader = anyio.create_memory_object_stream[types.JSONRPCMessage | Exception](0)
+        out_stream_writer, out_stream_reader = anyio.create_memory_object_stream[types.JSONRPCMessage | Exception](0)
+
+        # Internal streams
         sse_stream_writer, sse_stream_reader = anyio.create_memory_object_stream[Event](0)
 
         session_id = uuid4()
         session_uri = f"{quote(self._message_path)}?session_id={session_id.hex}"
-        self._read_stream_writers[session_id] = read_stream_writer
+        self._out_stream_writers[session_id] = out_stream_writer
         logger.debug("Created new session with ID: %s", session_id)
 
-        async def sse_writer() -> None:
+        async def _in_stream_processor() -> None:
+            """Redirect messages from the input stream to the SSE stream."""
             logger.debug("Starting SSE writer")
-            async with sse_stream_writer, write_stream_reader:
-                logger.debug("[sse_writer] Sending initial endpoint event")
-                await sse_stream_writer.send(
-                    Event(
-                        event="endpoint",
-                        data=session_uri,
-                    )
+            async with sse_stream_writer, in_stream_reader:
+                logger.debug("Sending initial endpoint event on startup")
+                event = Event(event_type=EventType.ENDPOINT, data=session_uri)
+                await sse_stream_writer.send(event)
+                logger.debug("Sent event: %s", event)
+
+                async for msg in in_stream_reader:
+                    data = self._ensure_string(msg)
+                    event = Event(event_type=EventType.MESSAGE, data=data)
+                    logger.debug("Sending event: %s", msg)
+                    await sse_stream_writer.send(event)
+                    logger.debug("Sent event: %s", event)
+
+        async def _response_writer() -> None:
+            """Redirect messages from the SSE stream to the response."""
+            logger.debug("Starting SSE stream processor")
+            async for event in sse_stream_reader:
+                logger.debug("Got event to send: %s", event)
+                with anyio.move_on_after(self._send_timeout) as cancel_scope:
+                    logger.debug("Sending event via SSE: %s", event)
+                    await response.send(data=event.data, event=event.event_type)
+                    logger.debug("Sent event via SSE: %s", event)
+
+                if cancel_scope and cancel_scope.cancel_called:
+                    await sse_stream_reader.aclose()
+                    raise TimeoutError()
+
+        async with sse_response(request) as response:
+            async with anyio.create_task_group() as tg:
+                # https://trio.readthedocs.io/en/latest/reference-core.html#custom-supervisors
+                async def cancel_on_finish(coro: Callable[[], Awaitable[None]]) -> None:
+                    await coro()
+                    tg.cancel_scope.cancel()
+
+                tg.start_soon(cancel_on_finish, _response_writer)
+                tg.start_soon(cancel_on_finish, _in_stream_processor)
+
+                yield SSEConnection(
+                    read_stream=out_stream_reader,
+                    write_stream=in_stream_writer,
+                    request=request,
+                    response=response,
                 )
-                logger.debug("[sse_writer] Sent endpoint event: %s", session_uri)
-
-                async for message in write_stream_reader:
-                    logger.debug("[sse_writer] Sending message via SSE: %s", message)
-
-                    if isinstance(message, types.JSONRPCMessage):
-                        data = message.model_dump_json(by_alias=True, exclude_none=True)
-                    else:
-                        data = str(message)
-
-                    await sse_stream_writer.send(Event(event="message", data=data))
-
-        # ----------------------------------------------------------------
-        async def _stream_response(request: web.Request) -> None:
-            async with sse_response(request) as response:
-                async for msg in sse_stream_reader:
-                    logger.debug("[_stream_response] chunk: %s", msg)
-                    with anyio.move_on_after(self._send_timeout) as cancel_scope:
-                        logger.debug("[_stream_response] Sending message via SSE: %s", msg)
-                        await response.send(data=msg.data, event=msg.event)
-                    logger.debug("[_stream_response] Sent message via SSE: %s", msg)
-
-                    if cancel_scope and cancel_scope.cancel_called:
-                        await sse_stream_reader.aclose()
-                        raise TimeoutError()
-
-        # ----------------------------------------------------------------
-        async with anyio.create_task_group() as tg:
-            logger.debug("Starting SSE response task")
-
-            # https://trio.readthedocs.io/en/latest/reference-core.html#custom-supervisors
-            async def cancel_on_finish(coro: Callable[[], Awaitable[None]]) -> None:
-                await coro()
-                tg.cancel_scope.cancel()
-
-            tg.start_soon(cancel_on_finish, lambda: _stream_response(request))
-            tg.start_soon(cancel_on_finish, sse_writer)
-            yield read_stream, write_stream
 
     async def handle_post_message(self, request: web.Request) -> web.Response:
         logger.debug("Handling POST message")
@@ -115,8 +138,8 @@ class SseServerTransport:
             logger.warning("Received invalid session ID: %s", session_id_param)
             return web.Response(text="Invalid session ID", status=400)
 
-        writer = self._read_stream_writers.get(session_id)
-        if not writer:
+        out_stream = self._out_stream_writers.get(session_id)
+        if not out_stream:
             logger.warning("Could not find session for ID: %s", session_id)
             return web.Response(text="Could not find session", status=404)
 
@@ -128,9 +151,9 @@ class SseServerTransport:
             logger.debug("Validated client message: %s", message)
         except ValidationError as err:
             logger.error("Failed to parse message: %s", err)
-            await writer.send(err)
+            await out_stream.send(err)
             return web.Response(text="Could not parse message", status=400)
 
         logger.debug("Sending message to writer: %s", message)
-        await writer.send(message)
+        await out_stream.send(message)
         return web.Response(text="Accepted", status=202)
