5ef47a5b668b19bf90949842175e602e31c96dce
Taras Drapalyuk <taras@drapalyuk.com>
2026-04-09 23:15:38 +0200

    refactor: import all public classes from their origin modules in __init__

--- aiohttp_mcp/__init__.py
+++ aiohttp_mcp/__init__.py
@@ -1,15 +1,9 @@
 from .app import AppBuilder, build_mcp_app, setup_mcp_subapp
 from .core import AiohttpMCP
 from .protocol.context import Context, get_current_context
-from .types import (
-    Annotations,
-    EventStore,
-    Icon,
-    Prompt,
-    Resource,
-    Tool,
-    ToolAnnotations,
-)
+from .protocol.messages import EventStore
+from .protocol.models import Annotations, Icon, Prompt, Resource, Tool, ToolAnnotations
+from .protocol.registry import ToolError
 
 __all__ = [
     "AiohttpMCP",
@@ -22,6 +16,7 @@ __all__ = [
     "Resource",
     "Tool",
     "ToolAnnotations",
+    "ToolError",
     "build_mcp_app",
     "get_current_context",
     "setup_mcp_subapp",
